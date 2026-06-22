b67e7fa298658b5fb7714740d13de88b715bbd92
Vedant Vatare <vedantvatare73@gmail.com>
2026-06-22 22:36:19 +0530

    fix(shared): specify auth method used for github node during oauth

--- packages/shared/src/credentials/providers/github.ts
+++ packages/shared/src/credentials/providers/github.ts
@@ -5,6 +5,7 @@ export const githubCredential: OAuth2CredentialDef = {
 	name: "github",
 	displayName: "GitHub",
 	icon: "github",
+	authMethod: "body",
 	authUrl: "https://github.com/login/oauth/authorize",
 	tokenUrl: "https://github.com/login/oauth/access_token",
 	scopes: ["repo", "user"],
